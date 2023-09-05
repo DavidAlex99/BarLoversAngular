9dd363dc3d41444cd1a4cc71df961c8f8cd3c9ae
Juan Guadalupe <jaguadal@espol.edu.ec>
2023-09-05 10:48:20 -0500

    arreglo estructura formularios

--- baresLovers/src/app/app.module.ts
+++ baresLovers/src/app/app.module.ts
@@ -12,8 +12,6 @@ import { DetallesBarComponent } from './components/detalles-bar/detalles-bar.com
 import { NewPostComponent } from './pages/new-post/new-post.component';
 import { NewEventComponent } from './pages/new-event/new-event.component';
 import { CuerpoNewPostComponent } from './components/cuerpo-new-post/cuerpo-new-post.component';
-import { MultimediaNewPostComponent } from './components/multimedia-new-post/multimedia-new-post.component';
-import { MultimediaNewEventComponent } from './components/multimedia-new-event/multimedia-new-event.component';
 import { VisualBarComponent } from './components/visual-bar/visual-bar.component';
 import { NewsComponent } from './components/news/news.component';
 import { VisualFoodComponent } from './components/visual-food/visual-food.component';
@@ -35,8 +33,6 @@ import { CuerpoNewEventComponent } from './components/cuerpo-new-event/cuerpo-ne
     NewPostComponent,
     CuerpoNewPostComponent,
     CuerpoNewEventComponent,
-    MultimediaNewPostComponent,
-    MultimediaNewEventComponent,
     VisualBarComponent,
     NewsComponent,
     VisualFoodComponent,
