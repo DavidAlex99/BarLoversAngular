98e34da95adba343258beebe7e4dc40c5396339d
Juan Guadalupe <jaguadal@espol.edu.ec>
2023-09-05 11:15:17 -0500

    modificaciones

--- baresLovers/src/app/components/cuerpo-new-event/cuerpo-new-event.component.ts
+++ baresLovers/src/app/components/cuerpo-new-event/cuerpo-new-event.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./cuerpo-new-event.component.css']
 })
 export class CuerpoNewEventComponent {
+  formularioData: any = {};
 
+  submitForm() {
+    // Aquí puedes enviar los datos del formulario al servidor utilizando un servicio HTTP.
+    // Ejemplo:
+    // this.miServicio.postData(this.formularioData).subscribe(response => {
+    //   console.log(response);
+    // });
+  }
+
+  goBack() {
+    // Aquí puedes agregar la lógica para volver a la página anterior.
+    // Ejemplo:
+    // this.router.navigate(['/pagina-anterior']);
+  }
 }
